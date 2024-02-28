382a731960faca7abb4fa05c78d68be9fdf23285
spdskatr <25078279+spdskatr@users.noreply.github.com>
2024-02-28 02:06:59 +0000

    Make puzzle

--- scenes/grid.gd
+++ scenes/grid.gd
@@ -16,17 +16,35 @@ const Gen = preload("res://scenes/logic/board_gen.gd")
 
 var DebugLines = []
 var Breadcrumbs = []
+var Items = []
+
+var points = 0
 
 # TODO: Maybe move out to its own script?
 var player_pos : Vector2i = Vector2i(0, 0)
 var player_pos_list : Array[Vector2i] = [Vector2i(0, 0)]
+var item_pos_list : Array[Vector2i] = [Vector2i(BOARD_SIZE-1, BOARD_SIZE-1)]
 
 func in_bounds(pos : Vector2i):
 	return 0 <= min(pos.x, pos.y) and max(pos.x, pos.y) < BOARD_SIZE
 
-func can_move(dir : Vector2i):
+func is_protected(pos : Vector2i) -> bool:
+	# Items cannot be collected out of order
+	var next_ind = -1
+	for i in range(len(item_pos_list)):
+		if item_pos_list[i] not in player_pos_list:
+			next_ind = i
+			break
+	var loc = item_pos_list.find(pos)
+	if loc <= next_ind:
+		return false
+	return true
+
+func do_move(dir : Vector2i) -> bool:
 	var new_pos = player_pos + dir
 	if in_bounds(new_pos):
+		if is_protected(new_pos):
+			return false
 		var idx = player_pos_list.find(new_pos)
 		# Make sure player is not looping in on themselves
 		if idx == -1:
@@ -38,6 +56,14 @@ func can_move(dir : Vector2i):
 			return true
 		return false
 	return false
+
+func puzzle_completed() -> bool:
+	if player_pos != item_pos_list[-1]:
+		return false
+	for item_pos in item_pos_list:
+		if item_pos not in player_pos_list:
+			return false
+	return true
 
 func update_player_position():
 	$Player.position = BOARD_BASE_POS + TILE_SIZE * Vector2(player_pos)
@@ -54,30 +80,46 @@ func update_player_position():
 		line.rotation = delta.angle()
 		Breadcrumbs.push_back(line)
 		add_child(line)
+	if puzzle_completed():
+		points += player_pos_list.size()
+		print("Points:", points)
+		debug_generator()
 
-func reset(new_board : Array[Vector2i]):
+func reset(new_board : Array[Vector2i], new_items : Array[Vector2i]):
 	for line in Breadcrumbs:
 		line.queue_free()
 	Breadcrumbs.clear()
 	player_pos = new_board[0]
 	player_pos_list = [new_board[0]]
+	item_pos_list = new_items
 	update_player_position()
 
 func debug_generator():
 	for line in DebugLines:
 		line.queue_free()
 	DebugLines.clear()
-	var config = Gen.new().generate(7)
-	for i in range(len(config) - 1):
-		var prev = config[i]
-		var next = config[i+1]
-		var delta = Vector2(next - prev)
-		var line = DebugBoardLine.instantiate()
-		line.position = BOARD_BASE_POS + TILE_SIZE * Vector2(prev)
-		line.rotation = delta.angle()
-		DebugLines.push_back(line)
-		add_child(line)
-	reset(config)
+	for item in Items:
+		item.queue_free()
+	Items.clear()
+	var gen = Gen.new()
+	var config = gen.generate(7)
+	#for i in range(len(config) - 1):
+	#	var prev = config[i]
+	#	var next = config[i+1]
+	#	var delta = Vector2(next - prev)
+	#	var line = DebugBoardLine.instantiate()
+	#	line.position = BOARD_BASE_POS + TILE_SIZE * Vector2(prev)
+	#	line.rotation = delta.angle()
+	#	DebugLines.push_back(line)
+	#	add_child(line)
+	var items = gen.get_items(config, 5)
+	var new_items : Array[Vector2i] = []
+	for loc in items.keys():
+		items[loc].position = BOARD_BASE_POS + TILE_SIZE * Vector2(loc)
+		new_items.push_back(loc)
+		add_child(items[loc])
+		Items.push_back(items[loc])
+	reset(config, new_items)
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -91,7 +133,7 @@ func _unhandled_input(event : InputEvent):
 	for k in INPUTS.keys():
 		if event.is_action_pressed(k):
 			var dir = INPUTS[k]
-			if can_move(dir):
+			if do_move(dir):
 				player_pos += dir
 				update_player_position()
 
--- scenes/logic/board_gen.gd
+++ scenes/logic/board_gen.gd
@@ -1,5 +1,20 @@
 extends Node
 
+const Beans = preload("res://scenes/entities/items/beans.tscn")
+const Sugar = preload("res://scenes/entities/items/sugar.tscn")
+const CoffeePot = preload("res://scenes/entities/items/coffee_pot.tscn")
+const Mug = preload("res://scenes/entities/items/mug.tscn")
+const Flower = preload("res://scenes/entities/items/flower.tscn")
+const Target = preload("res://scenes/entities/items/target.tscn")
+const Items = [
+	Beans,
+	Sugar,
+	CoffeePot,
+	Mug,
+	Flower,
+	
+]
+
 func is_adj(a : Vector2i, b : Vector2i) -> bool:
 	return (a - b).length_squared() == 1
 
@@ -26,14 +41,31 @@ func backbite(seq : Array[Vector2i]) -> Array[Vector2i]:
 
 func generate(size : int) -> Array[Vector2i]:
 	var board : Array[Vector2i] = generate_initial(size)
-	for _i in range(size * size * 10):
+	for _i in range(size * size * 20):
 		board = backbite(board)
 	board.reverse()
-	for _i in range(size * size * 10):
+	for _i in range(size * size * 20):
 		board = backbite(board)
 	for i in range(board.size() - 1):
 		assert(is_adj(board[i], board[i+1]))
 	return board
+
+func get_items(seq : Array[Vector2i], k : int) -> Dictionary:
+	# Place items at turns
+	var turns = []
+	for i in range(1, seq.size() - 1):
+		if seq[i-1] - seq[i] != seq[i] - seq[i+1]:
+			turns.push_back(i)
+	turns.shuffle()
+	# I hope there are at least k turns
+	assert(k <= turns.size())
+	var selected = turns.slice(0, k)
+	selected.sort()
+	var item_locs = {}
+	for i in range(k):
+		item_locs[seq[selected[i]]] = Items[i].instantiate()
+	item_locs[seq[-1]] = Target.instantiate()
+	return item_locs
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
